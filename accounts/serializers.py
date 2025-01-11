34d04c7137bcc507369a74bc5fead6ca3b893629
notashwinii <ashwinii.subedi@gmail.com>
2025-01-11 21:37:01 +0545

    add serializers for accounts app

---
+++ accounts/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from .models import CustomUser
+from django.contrib.auth import authenticate
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username', 'email', 'role']
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, attrs):
+        user = authenticate(username=attrs['username'], password=attrs['password'])
+        if user is None:
+            raise serializers.ValidationError("Invalid credentials")
+        return user
